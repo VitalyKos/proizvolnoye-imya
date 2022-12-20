60a70bca824c7c9f0959df22c16006a6f6ed65a6
VitalyKos <118600458+VitalyKos@users.noreply.github.com>
2022-12-20 18:22:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
 import random
 import sys
-
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
 from PyQt5 import uic  # Импортируем uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from ui_file import Ui_Form
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableView
 
-class MyWidget(QMainWindow, Ui_Form):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.pushButton.clicked.connect(self.run)
-        self.do_paint = False
-        # Обратите внимание: имя элемента такое же как в QTDesigner
-
-    def run(self):
-        self.do_paint = True
-        self.repaint()
-
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw_flag(qp)
-            qp.end()
-
-    def draw_flag(self, qp):
-        for _ in range(10):
-            qp.setBrush(QColor(*random.choices(range(256), k=3)))
-            size = random.randint(10, 100)
-            qp.drawEllipse(random.randint(0, self.width() - size), random.randint(0, self.height() - size), size, size)
+        uic.loadUi('main.ui', self)
+        db = QSqlDatabase.addDatabase('QSQLITE')
+        db.setDatabaseName('coffee.sqlite')
+        db.open()
+        model = QSqlTableModel(self, db)
+        model.setTable('coffee')
+        model.select()
+        self.tableView.setModel(model)
 
 
 if __name__ == '__main__':
